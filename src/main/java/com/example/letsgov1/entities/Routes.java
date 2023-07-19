67985e481143d7d2945c51aac8deee4112125322
jacob <118015063+jacobmccallum16@users.noreply.github.com>
2023-07-19 02:33:48 +0000

    Early Routes.java file

---
+++ src/main/java/com/example/letsgov1/entities/Routes.java
@@ -0,0 +1,27 @@
+package com.example.letsgov1.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Routes {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Integer routeId;
+    // Trips trips
+    String locationStart = "";
+    String locationEnd = "";
+    float routeDistance = 0f;
+    float routeDuration = 0f;
+    String routeStatus = "inactive";
+    // Integer[] requestedBy;
+    String routeImageSource;
+    @Column(columnDefinition = "FLOAT DEFAULT '0'") Float driverResponsibilityRating = 0f;
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP") Timestamp createdAt;
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP") Timestamp updatedAt;
+}
