c3d407ff0fd17837466fc920426a65caf60c3bf7
jacobmccallum16 <jacobmccallum16@gmail.com>
2023-07-24 15:46:28 -0700

    Visual Improvements

--- src/main/java/com/example/letsgo/repositories/UserRepository.java
+++ src/main/java/com/example/letsgo/repositories/UserRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
-    List<User> findUserByUserId (Integer keyId);
+    List<User> findUsersByUserId(Integer keyId);
+    User findUserByUserId(Integer keyId);
 }
